6229e32b78b19c829b3226c12d14b39a26cb0101
Momme Jürgensen <momme@juergensen.me>
2019-11-24 22:17:29 +0000

    upgraded AddressRainbow

--- api/src/modes/AddressRainbow.js
+++ api/src/modes/AddressRainbow.js
@@ -37,11 +37,13 @@ exports.class = class AddressRainbow extends Template {
   constructor(_width, _height, _config) {
     super(_width, _height, _config)
     this.Info = exports.Info
-    this.defaultConfig.repeat = 1
-    this.defaultConfig.fps = 8
+    // this.defaultConfig.repeat = 1
+    this.defaultConfig.fps = 10
     this.defaultConfig.shift = 1
     this.defaultConfig.direction = 'Y'
+    this.defaultConfig.cycleTime = 10000
 
+    this.p = 0
     this.matrix = []
 
     this.init()
@@ -49,7 +51,7 @@ exports.class = class AddressRainbow extends Template {
 
   setConfig (newConfig) {
     let reInit = false
-    if (newConfig.repeat && newConfig.repeat !== this.getConfig(newConfig.repeat)) reInit = true
+    // if (newConfig.repeat && newConfig.repeat !== this.getConfig(newConfig.repeat)) reInit = true
     if (newConfig.direction && newConfig.direction !== this.getConfig(newConfig.direction)) reInit = true
     super.setConfig(newConfig)
     if (reInit) this.init()
@@ -69,25 +71,22 @@ exports.class = class AddressRainbow extends Template {
       }
       this.matrix.push(column)
     }
-
-    if (this.getConfig('direction').toUpperCase() === 'Y') {
-      let frequency = this.height/600*Math.floor(this.getConfig('repeat'))
-      for (let x = 0; x < this.width; x++) {
-        for (let y = 0; y < this.height; y++) {
-          this.matrix[x][y] = {
-            r: Math.floor((Math.sin(frequency*y + 0) * 127 + 128)),
-            g: Math.floor((Math.sin(frequency*y + 2) * 127 + 128)),
-            b: Math.floor((Math.sin(frequency*y + 4) * 127 + 128)),
-            a: 1
-          }
-        }
-      }
-    }
+    this.p = 0
     super.init()
   }
 
   update () {
     if (this.getConfig('direction').toUpperCase() === 'Y') {
+      this.p += 1 / (this.getConfig('fps') * (this.getConfig('cycleTime') / 1000))
+      if (this.p >= 1) this.p = 0
+
+      let { r, g, b } = this.hslToRgb(this.p, 1, 0.5)
+      for (let x = 0; x < this.width; x++) {
+        if (this.getConfig('shift') > 0)
+          this.matrix[x][0] = { r, g, b, a: 1 }
+        else
+          this.matrix[x][this.height-1] = { r, g, b, a: 1 }
+      }
       this.shiftY()
     }
   }
@@ -96,17 +95,26 @@ exports.class = class AddressRainbow extends Template {
     let shift = this.getConfig('shift')
 
     for (let x = 0; x < this.width; x++) {
-      let oldColumn = this.matrix[x]
-      
-      for (let y = 0; y < this.height; y++) {
-        let color = [ 0, 0, 0 ]
-        if (shift < 0) {
-          color = y <= shift*-1 ? oldColumn[this.height-1] : oldColumn[y+shift]
-        } else if (shift > 0) {
-          color = y >= this.height-shift ? oldColumn[0] : oldColumn[y+shift]
+      if (shift > 0) {
+        let reverseCol = this.matrix[x].reverse()
+        for (let i = 0; i < reverseCol.length; i++) {
+          let rgba = this.matrix[x][i]
+          if (this.matrix[x][i+shift] !== undefined) {
+            rgba = this.matrix[x][i+shift]
+          }
+          reverseCol[i] = rgba
         }
-        this.matrix[x][y] = color
-        
+        this.matrix[x] = reverseCol.reverse()
+      } else if (shift < 0) {
+        let col = this.matrix[x]
+        for (let i = 0; i < col.length; i++) {
+          let rgba = this.matrix[x][i]
+          if (this.matrix[x][i+(shift*-1)] !== undefined) {
+            rgba = this.matrix[x][i+(shift*-1)]
+          }
+          col[i] = rgba
+        }
+        this.matrix[x] = col
       }
     }
   }
--- api/src/modes/FullRainbow.js
+++ api/src/modes/FullRainbow.js
@@ -46,11 +46,11 @@ exports.class = class FullRainbow extends Template {
     for (let x = 0; x < this.width; x++) {
       let column = []
       for (let y = 0; y < this.height; y++) {
-        let rgb = this.hslToRgb(this.p, 1, 0.5)
+        let { r, g, b } = this.hslToRgb(this.p, 1, 0.5)
         column.push({
-          r: rgb[0],
-          g: rgb[1],
-          b: rgb[2],
+          r,
+          g,
+          b,
           a: 1
         })
       }
@@ -59,32 +59,8 @@ exports.class = class FullRainbow extends Template {
     return res
   }
 
-  hslToRgb (h, s, l) {
-    var r, g, b;
-    if (s == 0) {
-      r = g = b = l // achromatic
-    } else {
-      var hue2rgb = function hue2rgb (p, q, t) {
-        if(t < 0) t += 1;
-        if(t > 1) t -= 1;
-        if(t < 1/6) return p + (q - p) * 6 * t;
-        if(t < 1/2) return q;
-        if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-        return p;
-      }
-  
-      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-      var p = 2 * l - q;
-      r = hue2rgb(p, q, h + 1/3);
-      g = hue2rgb(p, q, h);
-      b = hue2rgb(p, q, h - 1/3);
-    }
-  
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-  }
-
   update () {
-    this.p += this.getConfig('fps') / this.getConfig('cycleTime')
+    this.p += 1 / (this.getConfig('fps') * (this.getConfig('cycleTime') / 1000))
     if (this.p >= 1) this.p = 0
   }
 }
