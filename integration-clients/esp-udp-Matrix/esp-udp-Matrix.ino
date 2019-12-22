996d863d6a6c5c3079cd80115907bf860bb640f7
Momme Jürgensen <momme@juergensen.me>
2019-12-22 22:43:32 +0100

    edit esp-udp

--- integration-clients/esp-udp-Matrix/esp-udp-Matrix.ino
+++ integration-clients/esp-udp-Matrix/esp-udp-Matrix.ino
@@ -19,18 +19,21 @@ IPAddress ip(10,0,80,23);
 IPAddress gateway(10,0,0,1);
 IPAddress subnet(255,255,0,0);
 
-const char* host = "10.0.0.12";
-const uint16_t port = 33334;
+IPAddress host(10, 0, 0, 12);
+unsigned int port = 33334;
 unsigned int localUdpPort = 33333;
 
-String configString = "TYPE=MATRIX;NAME=Bed;WIDTH=2;HEIGHT=60;HOST=10.0.80.23;PORT=33333";
-int retryCountDown = 0;
+char configString[] = "CONF:TYPE=MATRIX;NAME=Bed;WIDTH=2;HEIGHT=60;HOST=10.0.80.23;PORT=33333";
+int resendConfigCountDown = 0;
+bool connected = false;
+bool pongHasArrived = false;
+int pingCheckCountDown = 0;
 
 WiFiClient client;
 WiFiUDP Udp;
 
-Adafruit_NeoPixel strip0(60, D1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip1(60, D2, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip0(60, 0, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip1(60, 2, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(115200);
@@ -57,6 +60,108 @@ void setup() {
 }
 
 void loop() {
+  if(Udp.parsePacket()) {
+    String payload = Udp.readStringUntil('\n');
+    if (payload == "PING") {
+      pongHasArrived = true;
+      Udp.beginPacket(host, port);
+      Udp.write("PONG:Bed");
+      Udp.endPacket();
+    } else if (payload.startsWith("UPDATE")) {
+      int pointIndex = payload.indexOf(":");
+      if (pointIndex == -1) return;
+      String newState = payload.substring(pointIndex+1);
+      bool updateStrip0 = false;
+      bool updateStrip1 = false;
+      
+      while (newState.length() > 0) {
+        int endOfVar = newState.indexOf(";");
+        String toInspect = newState.substring(0, endOfVar);
+        if (endOfVar == -1) newState = "";
+        else newState = newState.substring(endOfVar+1);
+        
+        // extract key val out of toInspect
+        int X = -1;
+        int Y = -1;
+        float R = -1;
+        float G = -1;
+        float B = -1;
+        
+        while (toInspect.length()) {
+          
+          int endOfSection = toInspect.indexOf("#");
+          String SectiontoInspect = toInspect.substring(0, endOfSection);
+          if (endOfSection == -1) toInspect = "";
+          else toInspect = toInspect.substring(endOfSection+1);
+          
+          int eqIndex = SectiontoInspect.indexOf("=");
+          String key = SectiontoInspect.substring(0, eqIndex);
+          String val = SectiontoInspect.substring(eqIndex+1, endOfSection);
+          if (key == "X") X = val.toInt();
+          else if (key == "Y") Y = val.toInt();
+          else if (key == "R") R = val.toFloat();
+          else if (key == "G") G = val.toFloat();
+          else if (key == "B") B = val.toFloat();
+          
+          if(X != -1 && Y != -1 && R != -1 && G != -1 && B != -1) {
+            
+            
+            Serial.print("X: ");
+            Serial.print(X);
+            Serial.print(" Y: ");
+            Serial.print(Y);
+            Serial.print(" R: ");
+            Serial.print(R);
+            Serial.print(" G: ");
+            Serial.print(G);
+            Serial.print(" B: ");
+            Serial.print(B);
+            Serial.println();
+            
+
+            if (X == 0) {
+              strip0.setPixelColor(Y, R, G, B);
+              updateStrip0 = true;
+              
+            } else if (X == 1) {
+              strip1.setPixelColor(Y, R, G, B);
+              updateStrip1 = true;
+            }
+          }
+        }
+      }
+
+      if (updateStrip0) strip0.show();
+      if (updateStrip1) strip1.show();
+    }
+  }
+
+  pingCheckCountDown--;
+  if (pingCheckCountDown <= 0) {
+    if (pongHasArrived) {
+      connected = true;
+      pongHasArrived = false;
+    } else {
+      connected = false;
+    }
+    pingCheckCountDown = 3000;
+  }
+  
+  if (!connected) {
+    if (resendConfigCountDown <= 0) {
+      Udp.beginPacket(host, port);
+      Udp.write(configString);
+      Udp.endPacket();
+      resendConfigCountDown = 15000;
+    } else {
+      resendConfigCountDown--;
+    }
+  }
+  
+
+  delay(1);
+  
+  /*
   if (client.connected()) {
     int packetSize = Udp.parsePacket();
     if (packetSize) {
@@ -95,7 +200,7 @@ void loop() {
           
           if(X != -1 && Y != -1 && R != -1 && G != -1 && B != -1) {
             
-            /*
+            
             Serial.print("X: ");
             Serial.print(X);
             Serial.print(" Y: ");
@@ -107,7 +212,7 @@ void loop() {
             Serial.print(" B: ");
             Serial.print(B);
             Serial.println();
-            */
+            
 
             if (X == 0) {
               strip0.setPixelColor(Y, R, G, B);
@@ -137,7 +242,7 @@ void loop() {
     retryCountDown--;
     if (retryCountDown <= 0) {
       Serial.println("Connect to server!");
-      if (client.connect(host, port)) {
+      if (connect()) {
         Serial.println("Connected!");
       } else {
         Serial.println("Connection failed! Retry in 5 seconds");
@@ -146,4 +251,5 @@ void loop() {
     }
   }
   delay(1);
+  */
 }
